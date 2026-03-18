09f6549e37684ca9111d3ec9c53619132291bf23
Zorava-create <Dazsmee@hotmail.co.uk>
2026-03-18 16:23:50 +0000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Zorava",
+  description: "Wedding photo sharing",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
