8a5d580695e38ff0f7acb42e5bb1495eb933c156
Zorava-create <Dazsmee@hotmail.co.uk>
2026-03-18 20:22:38 +0000

    Update page.js

--- app/upload/page.js
+++ app/upload/page.js
@@ -2,7 +2,7 @@ import { themes } from "../themes";
 
 // 👉 Change this to test different themes:
 // classic | blush | rustic | luxe | pride | winter | midnight
-const theme = themes.luxe;
+const theme = themes.rustic;
 
 export default function UploadPage() {
   return (
