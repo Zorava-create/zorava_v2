1d9829cc0f9efc7505a0c40795fae26108f3a577
Zorava-create <Dazsmee@hotmail.co.uk>
2026-03-19 03:04:18 +0000

    Update page.js

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,1 +1,27 @@
+"use client";
+import PhotoGrid from "../../components/PhotoGrid";
+import PhotoModal from "../../components/PhotoModal";
+import Slideshow from "../../components/Slideshow";
+import { themes }from "../themes";
 
+const theme = themes.classic;
+
+export default function GalleryPage() {
+  return ( 
+    <main style={styles.container}>
+    <h1 style={styles.title}>Gallery</h1>
+
+  <PhotoGrid />
+  <PhotoModal />
+  <Slideshow />
+    </main>
+  );
+}
+
+const styles = {
+  container: {
+    minHeight: "100vh",
+    background: theme.background,
+    padding: "20px",
+  },
+};
